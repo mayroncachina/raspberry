d8e35b2bbe54a247aca616d053398dc5186628f7
root <root@raspberrypi.(none)>
2014-10-02 17:12:19 +0000

    sencor de movimento

--- sensor-pi.py
+++ sensor-pi.py
@@ -1,16 +1,32 @@
 import time
 import RPi.GPIO as io
-io.setmode(io.BCM)
+io.setmode(io.BOARD)
 
-pir_pin = 18
-door_pin = 23
+pir_pin = 3
+led_pin = 5
 
 io.setup(pir_pin, io.IN)         # activate input
-io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
+io.setup(led_pin, io.OUT)
 
-while True:
-	if io.input(pir_pin):
-		print("PIR ALARM!")
-	if io.input(door_pin):
-		print("DOOR ALARM!")
-	time.sleep(0.5)
+def toggle_pin:
+	if io.
+
+try:
+	led_off = True
+	while True:
+		if led_off:
+			io.output(led_pin, 1)
+		else:
+			io.output(led_pin, 0)
+
+		if io.input(pir_pin):
+			led_off = False
+		else:
+			led_off = True
+		
+		time.sleep(0.1)
+		
+
+finally:
+
+	io.cleanup()
